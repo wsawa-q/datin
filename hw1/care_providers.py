35144b505a9196d0017d9a32cab55969cc97dad0
Sawa-q <kurinnaya.valeria@gmail.com>
2023-03-20 23:15:50 +0100

    Initial commit

---
+++ hw1/care_providers.py
@@ -0,0 +1,133 @@
+import csv
+import os
+
+from rdflib import Graph, BNode, Literal, Namespace
+from rdflib.namespace import QB, RDF, XSD
+
+NS = Namespace("https://sawa.github.io/ontology#")
+NSR = Namespace("https://sawa.github.io/resources/")
+RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+hashmap = {}
+absolute_path = os.path.dirname(__file__)
+
+
+def main():
+    data_as_csv = load_csv_file_as_object(absolute_path + "/care_providers.csv")
+    for data in data_as_csv:
+        data['measure'] = hashmap[(data['Obec'], data['DruhPece'])]
+    data_cube = as_data_cube(data_as_csv)
+    f = open(absolute_path + "/care_providers.ttl", "w")
+    f.write(data_cube.serialize(format="ttl"))
+    f.close()
+    
+
+def load_csv_file_as_object(file_path: str):
+    result = []
+    with open(file_path, "r") as stream:
+        reader = csv.reader(stream)
+        header = next(reader)  # Skip header
+        for line in reader:
+            if (line[5], line[29]) in hashmap.keys():
+                hashmap[(line[5], line[29])] += 1
+            else:
+                hashmap[(line[5], line[29])] = 1
+
+            result.append({key: value for key, value in zip(header, line)})
+    return result
+
+
+def as_data_cube(data):
+    result = Graph()
+    dimensions = create_dimensions(result)
+    measures = create_measure(result)
+    structure = create_structure(result, dimensions, measures)
+    dataset = create_dataset(result, structure)
+    create_observations(result, dataset, data)
+    return result
+
+
+def create_dimensions(collector: Graph):
+
+    county = NS.county
+    collector.add((county, RDF.type, RDFS.Property))
+    collector.add((county, RDF.type, QB.DimensionProperty))
+    collector.add((county, RDFS.label, Literal("Okres", lang="cs")))
+    collector.add((county, RDFS.label, Literal("County", lang="en")))
+    collector.add((county, RDFS.range, XSD.string))
+
+    region = NS.region
+    collector.add((region, RDF.type, RDFS.Property))
+    collector.add((region, RDF.type, QB.DimensionProperty))
+    collector.add((region, RDFS.label, Literal("Kraj", lang="cs")))
+    collector.add((region, RDFS.label, Literal("Region", lang="en")))
+    collector.add((region, RDFS.range, XSD.string))
+
+    field_of_care = NS.field_of_care
+    collector.add((field_of_care, RDF.type, RDFS.Property))
+    collector.add((field_of_care, RDF.type, QB.DimensionProperty))
+    collector.add((field_of_care, RDFS.label, Literal("OborPece", lang="cs")))
+    collector.add((field_of_care, RDFS.label, Literal("FieldOfCare", lang="en")))
+    collector.add((field_of_care, RDFS.range, XSD.string))
+
+    return [county, region, field_of_care]
+
+
+def create_measure(collector: Graph):
+
+    measure = NS.measure
+    collector.add((measure, RDF.type, RDFS.Property))
+    collector.add((measure, RDF.type, QB.MeasureProperty))
+    collector.add((measure, RDFS.label, Literal("Pocet", lang="cs")))
+    collector.add((measure, RDFS.label, Literal("Measure", lang="en")))
+    collector.add((measure, RDFS.range, XSD.integer))
+
+    return [measure]
+
+
+def create_structure(collector: Graph, dimensions, measures):
+
+    structure = NS.structure
+    collector.add((structure, RDF.type, QB.structure))
+
+    for dimension in dimensions:
+        component = BNode()
+        collector.add((structure, QB.component, component))
+        collector.add((component, QB.dimension, dimension))
+
+    for measure in measures:
+        component = BNode()
+        collector.add((structure, QB.component, component))
+        collector.add((component, QB.measure, measure))
+
+    return structure
+
+
+def create_dataset(collector: Graph, structure):
+
+    dataset = NSR.dataCubeInstance
+    collector.add((dataset, RDF.type, QB.DataSet))
+    collector.add((dataset, RDFS.label, Literal(
+        "Care Providers", lang="en")))
+    collector.add((dataset, QB.structure, structure))
+
+    return dataset
+
+
+def create_observations(collector: Graph, dataset, data):
+    for index, row in enumerate(data):
+        resource = NSR["observation-" + str(index).zfill(3)]
+        create_observation(collector, dataset, resource, row)
+
+
+def create_observation(collector: Graph, dataset, resource, data):
+    collector.add((resource, RDF.type, QB.Observation))
+    collector.add((resource, QB.dataSet, dataset))
+    collector.add((resource, NS.county, Literal(data["Okres"], lang="cs")))
+    collector.add((resource, NS.region, Literal(data["Kraj"], lang="cs")))
+    collector.add((resource, NS.field_of_care, Literal(data["NazevZarizeni"], lang="cs")))
+    collector.add((resource, NS.measure, Literal(
+        data["measure"], datatype=XSD.integer)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ hw1/population.py
@@ -0,0 +1,120 @@
+import csv
+import os
+
+from rdflib import Graph, BNode, Literal, Namespace
+from rdflib.namespace import QB, RDF, XSD
+
+NS = Namespace("https://sawa.github.io/ontology#")
+NSR = Namespace("https://sawa.github.io/resources/")
+RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+absolute_path = os.path.dirname(__file__)
+
+
+def main():
+    data_as_csv = load_csv_file_as_object(absolute_path + "/population.csv")
+    data_cube = as_data_cube(data_as_csv)
+    f = open(absolute_path + "/population.ttl", "w")
+    f.write(data_cube.serialize(format="ttl"))
+    f.close()
+
+
+def load_csv_file_as_object(file_path: str):
+    result = []
+    with open(file_path, "r") as stream:
+        reader = csv.reader(stream)
+        header = next(reader)  # Skip header
+        for line in reader:
+            if line[2] == 'DEM0004' and (line[5] == '100' or line[5] == '101'):
+                result.append({key: value for key, value in zip(header, line)})
+    return result
+
+
+def as_data_cube(data):
+    result = Graph()
+    dimensions = create_dimensions(result)
+    measures = create_measure(result)
+    structure = create_structure(result, dimensions, measures)
+    dataset = create_dataset(result, structure)
+    create_observations(result, dataset, data)
+    return result
+
+
+def create_dimensions(collector: Graph):
+
+    county = NS.county
+    collector.add((county, RDF.type, RDFS.Property))
+    collector.add((county, RDF.type, QB.DimensionProperty))
+    collector.add((county, RDFS.label, Literal("Okres", lang="cs")))
+    collector.add((county, RDFS.label, Literal("County", lang="en")))
+    collector.add((county, RDFS.range, XSD.string))
+
+    region = NS.region
+    collector.add((region, RDF.type, RDFS.Property))
+    collector.add((region, RDF.type, QB.DimensionProperty))
+    collector.add((region, RDFS.label, Literal("Kraj", lang="cs")))
+    collector.add((region, RDFS.label, Literal("Region", lang="en")))
+    collector.add((region, RDFS.range, XSD.string))
+
+    return [county, region]
+
+
+def create_measure(collector: Graph):
+
+    measure = NS.measure
+    collector.add((measure, RDF.type, RDFS.Property))
+    collector.add((measure, RDF.type, QB.MeasureProperty))
+    collector.add((measure, RDFS.label, Literal("StredniHodnota", lang="cs")))
+    collector.add((measure, RDFS.label, Literal("Mean", lang="en")))
+    collector.add((measure, RDFS.range, XSD.integer))
+
+    return [measure]
+
+
+def create_structure(collector: Graph, dimensions, measures):
+
+    structure = NS.structure
+    collector.add((structure, RDF.type, QB.structure))
+
+    for dimension in dimensions:
+        component = BNode()
+        collector.add((structure, QB.component, component))
+        collector.add((component, QB.dimension, dimension))
+
+    for measure in measures:
+        component = BNode()
+        collector.add((structure, QB.component, component))
+        collector.add((component, QB.measure, measure))
+
+    return structure
+
+
+def create_dataset(collector: Graph, structure):
+
+    dataset = NSR.dataCubeInstance
+    collector.add((dataset, RDF.type, QB.DataSet))
+    collector.add((dataset, RDFS.label, Literal(
+        "Population 2021", lang="en")))
+    collector.add((dataset, QB.structure, structure))
+
+    return dataset
+
+
+def create_observations(collector: Graph, dataset, data):
+    for index, row in enumerate(data):
+        resource = NSR["observation-" + str(index).zfill(3)]
+        create_observation(collector, dataset, resource, row)
+
+
+def create_observation(collector: Graph, dataset, resource, data):
+    collector.add((resource, RDF.type, QB.Observation))
+    collector.add((resource, QB.dataSet, dataset))
+    if data['vuzemi_cis'] == 100:
+        collector.add((resource, NS.county, Literal(data["vuzemi_txt"], lang="cs")))
+    else:
+        collector.add((resource, NS.region, Literal(data["vuzemi_txt"], lang="cs")))
+    collector.add((resource, NS.measure, Literal(
+        data["hodnota"], datatype=XSD.integer)))
+
+
+if __name__ == "__main__":
+    main()
