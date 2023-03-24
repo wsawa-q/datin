95a012219635a075e7b0465fa5b7cbbd4c6c879f
Sawa-q <kurinnaya.valeria@gmail.com>
2023-03-24 15:38:25 +0100

    population fix

--- hw1/population.py
+++ hw1/population.py
@@ -9,13 +9,23 @@ NSR = Namespace("https://sawa.github.io/resources/")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 absolute_path = os.path.dirname(__file__)
 
-
 def main():
+    hashmap = county_codelist_create()
     data_as_csv = load_csv_file_as_object(absolute_path + "/population.csv")
-    data_cube = as_data_cube(data_as_csv)
+    data_cube = as_data_cube(data_as_csv, hashmap)
     f = open(absolute_path + "/population.ttl", "w")
     f.write(data_cube.serialize(format="ttl"))
     f.close()
+
+
+def county_codelist_create():
+    result = {}
+    with open(absolute_path + "/county_codelist.csv", "r") as stream:
+        reader = csv.reader(stream)
+        next(reader)
+        for line in reader:
+            result[line[8]] = line[4]
+    return result
 
 
 def load_csv_file_as_object(file_path: str):
@@ -24,18 +34,18 @@ def load_csv_file_as_object(file_path: str):
         reader = csv.reader(stream)
         header = next(reader)  # Skip header
         for line in reader:
-            if line[2] == 'DEM0004' and (line[5] == '100' or line[5] == '101'):
+            if line[2] == 'DEM0004' and line[5] == '101':
                 result.append({key: value for key, value in zip(header, line)})
     return result
 
 
-def as_data_cube(data):
+def as_data_cube(data, hashmap):
     result = Graph()
     dimensions = create_dimensions(result)
     measures = create_measure(result)
     structure = create_structure(result, dimensions, measures)
     dataset = create_dataset(result, structure)
-    create_observations(result, dataset, data)
+    create_observations(result, dataset, data, hashmap)
     return result
 
 
@@ -99,19 +109,18 @@ def create_dataset(collector: Graph, structure):
     return dataset
 
 
-def create_observations(collector: Graph, dataset, data):
+def create_observations(collector: Graph, dataset, data, hashmap):
     for index, row in enumerate(data):
         resource = NSR["observation-" + str(index).zfill(3)]
-        create_observation(collector, dataset, resource, row)
+        create_observation(collector, dataset, resource, row, hashmap)
 
 
-def create_observation(collector: Graph, dataset, resource, data):
+def create_observation(collector: Graph, dataset, resource, data, hashmap):
     collector.add((resource, RDF.type, QB.Observation))
     collector.add((resource, QB.dataSet, dataset))
-    if data['vuzemi_cis'] == 100:
+    if data['vuzemi_cis'] == "101":
         collector.add((resource, NS.county, Literal(data["vuzemi_txt"], lang="cs")))
-    else:
-        collector.add((resource, NS.region, Literal(data["vuzemi_txt"], lang="cs")))
+        collector.add((resource, NS.region, Literal(hashmap[data["vuzemi_kod"]], lang="cs")))
     collector.add((resource, NS.measure, Literal(
         data["hodnota"], datatype=XSD.integer)))
 
