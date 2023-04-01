f7b125082fd537b9d02c58084f0056d4393e0c9f
Sawa-q <kurinnaya.valeria@gmail.com>
2023-04-01 15:37:03 +0200

    Initial commit

---
+++ hw2/airflow/dags/data-cubes.py
@@ -0,0 +1,72 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+
+import os
+
+from population import county_codelist_create, \
+    load_csv_file_as_object as load_population_csv, \
+    as_data_cube as population_datacube
+from care_providers import load_csv_file_as_object as load_care_providers_csv, \
+    as_data_cube as care_providers_datacube, \
+    hashmap
+
+# The actual tasks defined here will run in a different context from the
+# context of this script. Different tasks run on different workers at
+# different points in time, which means that this script cannot be used to
+# cross communicate between tasks. Note that for this purpose we have a more
+# advanced feature called XComs.
+
+Variable.set('root', './airflow/')
+output_path = Variable.get("output_path") + "output/"
+input_path = "./airflow/input/"
+
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
+def calculate_population():
+    hashmap = county_codelist_create(input_path)
+    data_as_csv = load_population_csv(input_path + "population.csv")
+    data_cube = population_datacube(data_as_csv, hashmap)
+    f = open(output_path + "population.ttl", "w")
+    f.write(data_cube.serialize(format="ttl"))
+    f.close()
+    
+def calculate_health_care():
+    data_as_csv = load_care_providers_csv(input_path + "care_providers.csv")
+    for data in data_as_csv:
+        data['measure'] = hashmap[(data['Obec'], data['DruhPece'])]
+    data_cube = care_providers_datacube(data_as_csv)
+    f = open(output_path + "health_care.ttl", "w")
+    f.write(data_cube.serialize(format="ttl"))
+    f.close()
+ 
+dag_args = {
+    "email": ["test@gmail.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    'retry_delay': timedelta(minutes=15)
+}
+
+with DAG(
+    dag_id="data-cubes",
+    default_args=dag_args,
+    start_date=datetime(2023, 3, 13),
+    schedule_interval=None,
+    catchup=False,
+    tags=["NDBI046"],    
+) as dag:
+        
+    task01 = PythonOperator(
+        task_id="population",
+        python_callable=calculate_population
+    )
+    
+    task02 = PythonOperator(
+        task_id="care_providers",
+        python_callable=calculate_health_care
+    )
+
+    task01, task02
---
+++ hw2/airflow/dags/population.py
@@ -0,0 +1,123 @@
+import csv
+import os
+
+from rdflib import Graph, BNode, Literal, Namespace
+from rdflib.namespace import QB, RDF, XSD
+
+NS = Namespace("https://sawa.github.io/ontology#")
+NSR = Namespace("https://sawa.github.io/resources/")
+RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+SDMX_DIMENSION = Namespace("http://purl.org/linked-data/sdmx/2009/dimension#")
+SDMX_MEASURE = Namespace("http://purl.org/linked-data/sdmx/2009/measure#")
+
+def county_codelist_create(path):
+    result = {}
+    with open(path + "/county_codelist.csv", "r") as stream:
+        reader = csv.reader(stream)
+        next(reader)
+        for line in reader:
+            result[line[8]] = line[4]
+    return result
+
+
+def load_csv_file_as_object(file_path: str):
+    result = []
+    with open(file_path, "r") as stream:
+        reader = csv.reader(stream)
+        header = next(reader)  # Skip header
+        for line in reader:
+            if line[2] == 'DEM0004' and line[5] == '101':
+                result.append({key: value for key, value in zip(header, line)})
+    return result
+
+
+def as_data_cube(data, hashmap):
+    result = Graph()
+    dimensions = create_dimensions(result)
+    measures = create_measure(result)
+    structure = create_structure(result, dimensions, measures)
+    dataset = create_dataset(result, structure)
+    create_observations(result, dataset, data, hashmap)
+    return result
+
+
+def create_dimensions(collector: Graph):
+
+    county = NS.county
+    collector.add((county, RDF.type, RDFS.Property))
+    collector.add((county, RDF.type, QB.DimensionProperty))
+    collector.add((county, RDFS.label, Literal("Okres", lang="cs")))
+    collector.add((county, RDFS.label, Literal("County", lang="en")))
+    collector.add((county, RDFS.range, XSD.string))
+    collector.add((county, RDFS.subPropertyOf, SDMX_DIMENSION.refArea))
+    collector.add((county, QB.concept, SDMX_DIMENSION.refArea))
+
+    region = NS.region
+    collector.add((region, RDF.type, RDFS.Property))
+    collector.add((region, RDF.type, QB.DimensionProperty))
+    collector.add((region, RDFS.label, Literal("Kraj", lang="cs")))
+    collector.add((region, RDFS.label, Literal("Region", lang="en")))
+    collector.add((region, RDFS.range, XSD.string))
+    collector.add((region, RDFS.subPropertyOf, SDMX_DIMENSION.refArea))
+    collector.add((region, QB.concept, SDMX_DIMENSION.refArea))
+
+    return [county, region]
+
+
+def create_measure(collector: Graph):
+
+    measure = NS.measure
+    collector.add((measure, RDF.type, RDFS.Property))
+    collector.add((measure, RDF.type, QB.MeasureProperty))
+    collector.add((measure, RDFS.label, Literal("StredniHodnota", lang="cs")))
+    collector.add((measure, RDFS.label, Literal("Mean", lang="en")))
+    collector.add((measure, RDFS.range, XSD.integer))
+    collector.add((measure, RDFS.subPropertyOf, SDMX_MEASURE.obsValue))
+    collector.add((measure, QB.concept, SDMX_MEASURE.obsValue))
+
+    return [measure]
+
+
+def create_structure(collector: Graph, dimensions, measures):
+
+    structure = NS.structure
+    collector.add((structure, RDF.type, QB.structure))
+
+    for dimension in dimensions:
+        component = BNode()
+        collector.add((structure, QB.component, component))
+        collector.add((component, QB.dimension, dimension))
+
+    for measure in measures:
+        component = BNode()
+        collector.add((structure, QB.component, component))
+        collector.add((component, QB.measure, measure))
+
+    return structure
+
+
+def create_dataset(collector: Graph, structure):
+
+    dataset = NSR.dataCubeInstance
+    collector.add((dataset, RDF.type, QB.DataSet))
+    collector.add((dataset, RDFS.label, Literal(
+        "Population 2021", lang="en")))
+    collector.add((dataset, QB.structure, structure))
+
+    return dataset
+
+
+def create_observations(collector: Graph, dataset, data, hashmap):
+    for index, row in enumerate(data):
+        resource = NSR["observation-" + str(index).zfill(3)]
+        create_observation(collector, dataset, resource, row, hashmap)
+
+
+def create_observation(collector: Graph, dataset, resource, data, hashmap):
+    collector.add((resource, RDF.type, QB.Observation))
+    collector.add((resource, QB.dataSet, dataset))
+    if data['vuzemi_cis'] == "101":
+        collector.add((resource, NS.county, Literal(data["vuzemi_txt"], lang="cs")))
+        collector.add((resource, NS.region, Literal(hashmap[data["vuzemi_kod"]], lang="cs")))
+    collector.add((resource, NS.measure, Literal(
+        data["hodnota"], datatype=XSD.integer)))
